8120acdcb2a1d189e3575d256007e81277a91730
nabil016 <153996513+nabil016@users.noreply.github.com>
2024-06-01 18:59:10 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function appendValue(value) {
+    document.getElementById('display').value += value;
+}
+
+function calculate() {
+    try {
+        document.getElementById('display').value = eval(document.getElementById('display').value);
+    } catch (error) {
+        alert('Invalid calculation');
+    }
+}
